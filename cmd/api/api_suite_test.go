a3e8de1e86a43297342f3a753dfb7dc32c2fce9e
Gabriel Pozo <jackgris2@gmail.com>
2023-02-07 21:05:37 -0300

    add: first test to handlers
    Maybe should be better mock db

---
+++ cmd/api/api_suite_test.go
@@ -0,0 +1,44 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/jackgris/goscrapy/config"
+	"github.com/jackgris/goscrapy/database"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus"
+)
+
+func TestApi(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Api Suite")
+}
+
+var _ = BeforeSuite(func() {
+
+	log := logrus.New()
+	log.SetLevel(logrus.WarnLevel)
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp:          true,
+		TimestampFormat:        "2006-01-02 15:04:05",
+		ForceColors:            true,
+		DisableLevelTruncation: true,
+	})
+
+	// Getting all config needed for connections and pages login
+	setup := config.Get("../../data.env", log)
+
+	var err error
+	// Starting DB connection with a test database
+	_, err = database.Connect(setup.Dburi, setup.Dbuser,
+		setup.Dbpass, "mayorista2", log)
+	if err != nil {
+		log.Panicf("Error database connection %s", err.Error())
+	}
+	log.Warn("Established database connection")
+})
+
+var _ = AfterSuite(func() {
+	database.Disconnect()
+})
