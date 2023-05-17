43a32cb54976acf1b505456b04a04a8eec58ceb6
Gabriel Pozo <jackgris2@gmail.com>
2023-05-16 21:34:32 -0300

    fix: the first integration test now truly check the responses

--- cmd/api/handlers/v1/handlers_test.go
+++ cmd/api/handlers/v1/handlers_test.go
@@ -1,14 +1,19 @@
 package v1_test
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"runtime/debug"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/jackgris/goscrapy/business/database"
 	"github.com/jackgris/goscrapy/business/dbtest"
 	. "github.com/jackgris/goscrapy/cmd/api/handlers/v1"
+	v1 "github.com/jackgris/goscrapy/cmd/api/handlers/v1"
+	"github.com/jackgris/goscrapy/config"
 )
 
 func TestHome(t *testing.T) {
@@ -16,13 +21,16 @@ func TestHome(t *testing.T) {
 	app := fiber.New()
 	app.Get("/", Home)
 
+	response := struct{ Message string }{Message: "THIS HOME"}
+
 	tests := []struct {
 		name   string
 		url    string
 		method string
 		status int
+		resp   struct{ Message string }
 	}{
-		{name: "good", url: "/", method: "GET", status: http.StatusOK},
+		{name: "good", url: "/", method: "GET", status: http.StatusOK, resp: response},
 		{name: "bad", url: "/bad", method: "GET", status: http.StatusNotFound},
 		{name: "other method", url: "/", method: "POST", status: http.StatusMethodNotAllowed},
 	}
@@ -39,6 +47,18 @@ func TestHome(t *testing.T) {
 			if resp.StatusCode != tt.status {
 				t.Errorf("Home() status should be = %v, but get status %v", tt.status, resp.StatusCode)
 			}
+
+			if resp.StatusCode == http.StatusOK {
+				body, _ := ioutil.ReadAll(resp.Body)
+				var r struct{ Message string }
+				err := json.Unmarshal(body, &r)
+				if err != nil {
+					t.Errorf("Home() body should be like this { Message string } instead we have %s", r)
+				}
+				if r != response {
+					t.Errorf("Home() body should be = %v, but get %v", r, response)
+				}
+			}
 		})
 	}
 }
@@ -54,9 +74,21 @@ func TestGetAllProducts(t *testing.T) {
 		}
 		test.Teardown()
 	}()
+	setup := config.Data{
+		Dburi:  "mongodb://127.0.0.1:27017",
+		Dbuser: "admin",
+		Dbpass: "admin",
+	}
 
 	app := fiber.New()
-	app.Get("/", GetAllProducts(*cfg))
+
+	cfg := v1.Config{
+		Log:   log,
+		Db:    db,
+		Setup: &setup,
+	}
+
+	app.Get("/", GetAllProducts(cfg))
 
 	tests := []struct {
 		name   string
@@ -65,8 +97,8 @@ func TestGetAllProducts(t *testing.T) {
 		status int
 	}{
 		{name: "good", url: "/", method: "GET", status: http.StatusOK},
-		// {name: "bad", url: "/bad", method: "GET", status: http.StatusNotFound},
-		// {name: "other method", url: "/", method: "POST", status: http.StatusMethodNotAllowed},
+		{name: "bad", url: "/bad", method: "GET", status: http.StatusNotFound},
+		{name: "other method", url: "/", method: "POST", status: http.StatusMethodNotAllowed},
 	}
 
 	for _, tt := range tests {
@@ -77,10 +109,27 @@ func TestGetAllProducts(t *testing.T) {
 			// Perform the request plain with the app,
 			// the second argument is a request latency
 			// (set to -1 for no latency)
-			resp, _ := app.Test(req, 1)
+			resp, err := app.Test(req, -1)
+			if err != nil {
+				t.Errorf("GetAllProducts test: %s", err)
+			}
 
 			if resp.StatusCode != tt.status {
 				t.Errorf("GetAllProducts status should be = %v, but get status %v", tt.status, resp.StatusCode)
+			}
+
+			if resp.StatusCode == http.StatusOK {
+				body, _ := ioutil.ReadAll(resp.Body)
+				products := []database.Product{}
+
+				err := json.Unmarshal(body, &products)
+				if err != nil {
+					t.Errorf("GetAllProducts body should be []database.Product instead we have %s", body)
+				}
+				number := 1154
+				if len(products) != number {
+					t.Errorf("GetAllProducts number of products should be = %d, but get %d", number, len(products))
+				}
 			}
 		})
 
--- cmd/api/handlers/v1/main_test.go
+++ cmd/api/handlers/v1/main_test.go
@@ -10,10 +10,13 @@ import (
 	"github.com/jackgris/goscrapy/config"
 	"github.com/jackgris/goscrapy/foundation/docker"
 	logs "github.com/jackgris/goscrapy/foundation/logger"
+	"github.com/sirupsen/logrus"
 )
 
 var c *docker.Container
 var cfg *v1.Config
+var log *logrus.Logger
+var db *database.MongoDb
 
 func TestMain(m *testing.M) {
 
@@ -25,22 +28,21 @@ func TestMain(m *testing.M) {
 	}
 	defer dbtest.StopDB(c)
 
-	log := logs.New()
+	log = logs.New()
 
 	// Getting all config needed for connections and pages login
 	setup := config.Data{
-		Dburi:  "mongodb://" + c.Host, // "mongodb://127.0.0.1:27017",
+		Dburi:  "mongodb://" + c.Host,
 		Dbuser: "admin",
 		Dbpass: "admin",
 	}
 
 	// Starting DB connection
-	db, err := database.Connect(setup.Dburi, "mayorista2", log)
+	db, err = database.Connect(setup.Dburi, "mayorista", log)
 	if err != nil {
 		panic("Error database connection: " + err.Error())
 	}
 
-	defer database.Disconnect()
 	cfg = &v1.Config{
 		Log:   log,
 		Db:    db,
